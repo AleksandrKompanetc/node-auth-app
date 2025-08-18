b7b8160b8b082ae8035667a2a307ed480cb871dd
AleksandrKompanetc <moneymacker7@gmail.com>
2025-08-18 16:29:40 +0300

    added main page

--- app.js
+++ app.js
@@ -4,3 +4,16 @@ const path = require('path');
 const url = require('url');
 
 const users = require('./users');
+
+http.createServer(function (req, res) {
+  const url = req.url;
+  console.log(url);
+
+  switch (url) {
+    case '/':
+      console.log('main page');
+      staticFile(res, '/html/main.html', '.html');
+      break;
+  }
+
+}).listen(3500);
