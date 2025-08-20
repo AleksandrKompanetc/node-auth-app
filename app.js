ceacd08097341b85bf972634574cec321ffe30e7
AleksandrKompanetc <moneymacker7@gmail.com>
2025-08-20 23:00:18 +0300

    added async fetch request

--- app.js
+++ app.js
@@ -1,88 +1,101 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const url = require('url');
+// const http = require('http');
+// const fs = require('fs');
+// const path = require('path');
+// const url = require('url');
 
-const users = require('./users');
+// const users = require('./users');
 
-const mimeTypes = {
-    '.html': 'text/html',
-    '.css': 'text/css',
-    '.js': 'text/javascript',
-    '.png': 'image/png',
-    '.jpg': 'image/jpeg',
-    '.gif': 'image/gif',
-    '.svg': 'image/svg+xml',
-    '.ico': 'image/x-icon',
-    '.json': 'application/json',
-    '.mp3': 'audio/mpeg',
-    '.mp4': 'video/mp4',
-    '.txt': 'text/plain',
-    '.pdf': 'application/pdf',
-    '.doc': 'application/msword',
-    '.docx': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-    '.xls': 'application/vnd.ms-excel',
-    '.xlsx': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-    '.woff': 'application/font-woff',
-    '.woff2': 'application/font-woff2',
-    '.ttf': 'application/font-ttf',
-    '.eot': 'application/vnd.ms-fontobject',
-    '.otf': 'application/font-otf',
-    '.swf': 'application/x-shockwave-flash',
-    '.wasm': 'application/wasm',
-}
+// const mimeTypes = {
+//     '.html': 'text/html',
+//     '.css': 'text/css',
+//     '.js': 'text/javascript',
+//     '.png': 'image/png',
+//     '.jpg': 'image/jpeg',
+//     '.gif': 'image/gif',
+//     '.svg': 'image/svg+xml',
+//     '.ico': 'image/x-icon',
+//     '.json': 'application/json',
+//     '.mp3': 'audio/mpeg',
+//     '.mp4': 'video/mp4',
+//     '.txt': 'text/plain',
+//     '.pdf': 'application/pdf',
+//     '.doc': 'application/msword',
+//     '.docx': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+//     '.xls': 'application/vnd.ms-excel',
+//     '.xlsx': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+//     '.woff': 'application/font-woff',
+//     '.woff2': 'application/font-woff2',
+//     '.ttf': 'application/font-ttf',
+//     '.eot': 'application/vnd.ms-fontobject',
+//     '.otf': 'application/font-otf',
+//     '.swf': 'application/x-shockwave-flash',
+//     '.wasm': 'application/wasm',
+// }
 
-function staticFile(res, filePath, ext) {
-  res.setHeader('Content-Type', mimeTypes[ext]);
-  fs.readFile('./public'+filePath, (error, data) => {
-    if (error) {
-      res.statusCode = 404;
-      res.end();
+// function staticFile(res, filePath, ext) {
+//   res.setHeader('Content-Type', mimeTypes[ext]);
+//   fs.readFile('./public'+filePath, (error, data) => {
+//     if (error) {
+//       res.statusCode = 404;
+//       res.end();
+//     }
+//     res.end(data);
+//   })
+// }
+
+// http.createServer(function (req, res) {
+//   let url = req.url;
+//   console.log(url);
+
+//   switch (url) {
+//     case '/':
+//       console.log('main page');
+//       staticFile(res, '/html/main.html', '.html');
+//       break;
+//     case '/about':
+//       console.log('about page');
+//       staticFile(res, '/html/about.html', '.html');
+//       break;
+//     case '/contact':
+//       console.log('contact page');
+//       staticFile(res, '/html/contact.html', '.html');
+//       break;
+//     case '/admin':
+//       console.log('admin page');
+//       staticFile(res, '/html/admin.html', '.html');
+//       break;
+//     case '/login':
+//       console.log('login page');
+//       staticFile(res, '/html/login.html', '.html');
+//       break;
+//     case '/cabinet':
+//       console.log('checkuser');
+
+//       break;
+//     default:
+//       const extname = String(path.extname(url)).toLocaleLowerCase();
+//       if (extname in mimeTypes) {
+//         staticFile(res, url, extname);
+//       } else {
+//         res.statusCode = 404;
+//         res.end();
+//       }
+//   }
+
+// }).listen(3711);
+
+async function t1 () {
+  const res = await fetch('https://api.itgid.info/api/12/get-time', {
+    'method' : 'GET',
+    'headers' : {
+      'apikey' : 'Bm1xnk70pfWj4wVR',
     }
-    res.end(data);
-  })
+  });
+  const data = await res.json();
+  console.log(data);
 }
 
-http.createServer(function (req, res) {
-  let url = req.url;
-  console.log(url);
-
-  switch (url) {
-    case '/':
-      console.log('main page');
-      staticFile(res, '/html/main.html', '.html');
-      break;
-    case '/about':
-      console.log('about page');
-      staticFile(res, '/html/about.html', '.html');
-      break;
-    case '/contact':
-      console.log('contact page');
-      staticFile(res, '/html/contact.html', '.html');
-      break;
-    case '/admin':
-      console.log('admin page');
-      staticFile(res, '/html/admin.html', '.html');
-      break;
-    case '/login':
-      console.log('login page');
-      staticFile(res, '/html/login.html', '.html');
-      break;
-    case '/cabinet':
-      console.log('checkuser');
-
-      break;
-    default:
-      const extname = String(path.extname(url)).toLocaleLowerCase();
-      if (extname in mimeTypes) {
-        staticFile(res, url, extname);
-      } else {
-        res.statusCode = 404;
-        res.end();
-      }
-  }
-
-}).listen(3711);
+t1();
 
 // const https = require('follow-redirects').https;
 // const request = require('request');
